fa7308648feedba88d5ff5b57d3bc1500f40a859
Swetathakare <143093330+Swetathakare@users.noreply.github.com>
2023-11-29 02:21:03 +0530

    Update logindemo.java

--- logindemo.java
+++ logindemo.java
@@ -1,67 +1,116 @@
 package project;
 
-import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.GridLayout;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.concurrent.CountDownLatch;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JPasswordField;
-import javax.swing.JTextField;
+import javax.swing.border.LineBorder;
 
-class LoginDemo extends JFrame implements ActionListener {
-	   JPanel panel;
-	   JLabel user_label, password_label, message;
-	   JTextField userName_text;
-	   JPasswordField password_text;
-	   JButton submit, cancel;
-	   LoginDemo() {
-	      // Username Label
-	      user_label = new JLabel();
-	      user_label.setText("User Name :");
-	      userName_text = new JTextField();
-	      // Password Label
-	      password_label = new JLabel();
-	      password_label.setText("Password :");
-	      password_text = new JPasswordField();
-	      // Submit
-	      submit = new JButton("SUBMIT");
-	      panel = new JPanel(new GridLayout(3, 1));
-	      panel.add(user_label);
-	      panel.add(userName_text);
-	      panel.add(password_label);
-	      panel.add(password_text);
-	      message = new JLabel();
-	      panel.add(message);
-	      panel.add(submit);
-	      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	      // Adding the listeners to components..
-	      submit.addActionListener(this);
-	      add(panel, BorderLayout.CENTER);
-	      setTitle("Please Login Here !");
-	      Toolkit toolkit = Toolkit.getDefaultToolkit();
 
-	        // Get the screen's resolution
-	        Dimension screenResolution = new Dimension(toolkit.getScreenSize());
 
-	        // Set the frame's size to match the screen resolution
-	        setSize(screenResolution);
-	      setVisible(true);
-	   }
-	   
-	   @Override
-	  public void actionPerformed(ActionEvent ae){
-	      String userName = userName_text.getText();
-	      String password = password_text.getText();
-	      if (userName.trim().equals("admin") && password.trim().equals("admin")) {
-	         message.setText(" Hello " + userName + "");
-	      } else {
-	         message.setText(" Invalid user.. ");
-	      }
-	   }
-	}
+ class Myframe extends JFrame{
+    static boolean admn = false;
+	JFrame frame;
+	static CountDownLatch latch = new CountDownLatch(1); 
+    Myframe(){
+	   admn=false;
+        // Create a JFrame
+    	new JFrame();
+    	latch = new CountDownLatch(1);
+        setTitle("User and Admin Frame");
+        getContentPane().setBackground(new Color(173, 216, 230));
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        setSize(screenSize);
+        Color g = new Color(255,0,0);
+        
+     
+        JPanel mainPanel = new JPanel(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(20, 20, 20, 20); // Add padding
+
+        // Create the User label
+        JLabel userLabel = new JLabel("I Am Here To Learn!");
+        userLabel.setForeground(Color.BLACK); // Set label text color to black
+        userLabel.setFont(new Font("Serif", Font.ITALIC, 35)); // Increase font size
+
+        // Create the User button
+        JButton userButton = new JButton("User");
+        userButton.setBackground(new Color(0, 51, 102)); // Dark blue background
+        userButton.setForeground(Color.WHITE); // Set button text color to white
+        userButton.setFont(new Font("Arial", Font.PLAIN, 24)); // Increase font size
+
+        // Add action listener to the User button
+       
+        // Create the Admin label
+        JLabel adminLabel = new JLabel("I Am Here To Contribute !");
+        adminLabel.setForeground(Color.BLACK); // Set label text color to black
+        adminLabel.setFont(new Font("Serif", Font.ITALIC, 35)); // Increase font size
+
+        // Create the Admin button
+        JButton adminButton = new JButton("Admin");
+        adminButton.setBackground(new Color(0, 51, 102)); // Dark blue background
+        adminButton.setForeground(Color.WHITE); // Set button text color to white
+        adminButton.setFont(new Font("Arial", Font.PLAIN, 24)); // Increase font size
+
+        
+
+        // Add the User label and button to the main panel
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        mainPanel.add(userLabel, gbc);
+
+        gbc.gridy = 1;
+        mainPanel.add(userButton, gbc);
+
+        // Add the Admin label and button to the main panel
+        gbc.gridy = 2;
+        mainPanel.add(adminLabel, gbc);
+
+        gbc.gridy = 3;
+        mainPanel.add(adminButton, gbc);
+        LineBorder lb = new LineBorder(new Color(15, 8, 61),5);
+        mainPanel.setBorder(lb);
+        mainPanel.setBackground(new Color(202, 206, 217));
+        // Add the main panel to the frame
+        add(mainPanel);
+        
+        // Center the frame on the screen
+        setLocationRelativeTo(null);
+
+      ActionListener button1Listener = new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+      	admn=true;
+        
+          latch.countDown();
+          setVisible(false);
+          
+      }
+  };
+  ActionListener button2Listener = new ActionListener() {          
+      public void actionPerformed(ActionEvent e) {
+          latch.countDown();
+          setVisible(false);
+      }
+  };
+  adminButton.addActionListener(button1Listener);
+  userButton.addActionListener(button2Listener);   
+        setVisible(true);
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }}
